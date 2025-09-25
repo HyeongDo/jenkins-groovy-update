b88aed7b0c4120cf96498806f90719738f7d3501
HyeongDoShim <hd.shim@okestro.com>
2025-09-25 14:00:15 +0900

    fix

--- vars/main.groovy
+++ vars/main.groovy
@@ -2,12 +2,6 @@ import ci.Stages
 
 def call(Map config) {
     pipeline {
-        agent {
-            docker {
-                image 'docker:24.0.7-dind'
-                args '--privileged -v /var/run/docker.sock:/var/run/docker.sock'
-            }
-        }
         stages {
             stage('Checkout') {
                 steps {
